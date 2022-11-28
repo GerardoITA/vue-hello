2082d9590a465429d96ecf03088e02cde00d1eb1
GerardoITA <gerardo.giusti@tiscali.it>
2022-11-28 11:02:20 +0100

    .

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+            immagine: `https://doctorvet.it/wp-content/uploads/2019/11/the-lucky-neko-rplhB9mYF48-unsplash.jpg`
+            
         }
     }
 }).mount('#app') 
